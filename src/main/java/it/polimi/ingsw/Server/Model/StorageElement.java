7bf10b10f9c646d00a5bd785ad395e0f6a5651c0
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-03-29 11:29:53 +0200

    Methods cleanup to have a baseline for personalBoard interactions

--- src/main/java/it/polimi/ingsw/Server/Model/StorageElement.java
+++ src/main/java/it/polimi/ingsw/Server/Model/StorageElement.java
@@ -2,9 +2,21 @@ package it.polimi.ingsw.server.model;
 
 import java.util.*;
 
-
+/**
+ * Implemented by all elements that store resources.
+ * No method to add resources is present because it
+ * is not always implemented, for example depots only
+ * take a new arrangement, they do not add on top of
+ * what is previously inside.
+ */
 public interface StorageElement {
-    void discardResources(Map<Resource, Integer> delResources);
+    Map<Resource, Integer> getResources();
 
-    Map<Resource, Integer> peekResources();
+    void discardResources(Map<Resource, Integer> deletedResources);
+
+    default boolean hasResource(Resource checkedResource){
+        int amount = Optional.ofNullable(getResources().get(checkedResource))
+                .orElse(0);
+        return amount > 0;
+    }
 }
