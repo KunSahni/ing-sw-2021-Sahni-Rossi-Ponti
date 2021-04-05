5b13f32d0244af5b9d68946feafc2da6ba6d0cf9
AlessandroPonti99 <alessandro2.ponti@mail.polimi.it>
2021-04-06 01:26:13 +0200

    fixed all errors of development cards and Color

--- src/main/java/it/polimi/ingsw/Server/Model/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/Server/Model/DevelopmentCard.java
@@ -8,26 +8,12 @@ import java.time.*;
 
 public class DevelopmentCard implements VictoryPointsElement {
     private Color type;
-
     private Level level;
-
     private int victoryPoints;
-
     private Map<Resource, Integer> inputResources;
-
     private Map<Resource, Integer> outputResources;
-
     private Map<Resource, Integer> cost;
-
     private int faithIncrement;
-
-    private DevelopmentCardsBoard ;
-
-    private DevelopmentCardsDeck ;
-
-    private ResourceBank ;
-
-    private Color ;
 
 
     public Color getType() {
@@ -36,10 +22,6 @@ public class DevelopmentCard implements VictoryPointsElement {
 
     public Level getLevel() {
         return level;
-    }
-
-    public int getVictoryPoints() {
-        return victoryPoints;
     }
 
     public Map<Resource, Integer> getInputResources() {
@@ -58,124 +40,9 @@ public class DevelopmentCard implements VictoryPointsElement {
         return faithIncrement;
     }
 
-    public DevelopmentCardsBoard get() {
-        return ;
-    }
-
-    public DevelopmentCardsDeck get() {
-        return ;
-    }
-
-    public ResourceBank get() {
-        return ;
-    }
-
-    public Color get() {
-        return ;
-    }
-
-    public Level get() {
-        return ;
-    }
-
-    public void set(DevelopmentCardsBoard ) {
-        this. = ;
-    }
-
-    public void set(DevelopmentCardsDeck ) {
-        this. = ;
-    }
-
-    public void set(ResourceBank ) {
-        this. = ;
-    }
-
-    public void set(Color ) {
-        this. = ;
-    }
-
-    public void set(Level ) {
-        this. = ;
-    }
-
-    public void link(DevelopmentCardsBoard _) {
-        if (_ != null) {
-            _.unlink();
-            _.set(this);
-        }
-
-        unlink();
-        set(_);
-    }
-
-    public void link(DevelopmentCardsDeck _) {
-        if (_ != null) {
-            _.getDeck().add(this);
-        }
-
-        unlink();
-        set(_);
-    }
-
-    public void link(ResourceBank _) {
-        set(_);
-    }
-
-    public void link(Color _) {
-        if (_ != null) {
-            _.unlink();
-            _.set(this);
-        }
-
-        unlink();
-        set(_);
-    }
-
-    public void link(Level _) {
-        if (_ != null) {
-            _.unlink();
-            _.set(this);
-        }
-
-        unlink();
-        set(_);
-    }
-
-    public void unlink() {
-        if (get() != null) {
-            get().set(null);
-            set(null);
-        }
-    }
-
-    public void unlink() {
-        if (get() != null) {
-            get().getDeck().remove(this);
-            set(null);
-        }
-    }
-
-    public void unlink() {
-        set(null);
-    }
-
-    public void unlink() {
-        if (get() != null) {
-            get().set(null);
-            set(null);
-        }
-    }
-
-    public void unlink() {
-        if (get() != null) {
-            get().set(null);
-            set(null);
-        }
-    }
-
     @Override
     public int getVictoryPoints() {
-
+        return victoryPoints;
     }
 
     /**
@@ -183,13 +50,9 @@ public class DevelopmentCard implements VictoryPointsElement {
      * move the marker on the Faith Track
      * @return the resources it produces
      */
-    public Map<Resource, Integer> produce() {
-        int i;
-
-        StorageElement.discardResources(inputResources);
-        StorageElement.storeResources(outputResources);
-        for(i=0; i<faithIncrement; i++) FaithTrack.moveMarker();
-        return outputResources;
+    public ProductionOutput produce() {
+        ProductionOutput productionOutput = new ProductionOutput(faithIncrement, outputResources);
+        return productionOutput;
     }
 
 }
