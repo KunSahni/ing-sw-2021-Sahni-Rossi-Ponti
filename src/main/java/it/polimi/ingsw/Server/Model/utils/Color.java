9ce81ef9a0bad396ca005890569466e3488d63e0
AlessandroPonti99 <alessandro2.ponti@mail.polimi.it>
2021-04-15 17:12:37 +0200

    Re-created level and color enumerations

---
+++ src/main/java/it/polimi/ingsw/Server/Model/utils/Color.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.server.model.utils;
+
+public enum Color {
+    GREEN("Green"),
+    BLUE("Blue"),
+    YELLOW("Yellow"),
+    PURPLE("Purple");
+
+    public final String label;
+
+    Color(String label) {
+        this.label = label;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Server/Model/utils/Level.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.server.model.utils;
+
+public enum Level {
+    LEVEL1("Level1"),
+    LEVEL2("Level2"),
+    LEVEL3("Level3");
+
+    public final String label;
+
+    Level(String label) {
+        this.label = label;
+    }
+}
