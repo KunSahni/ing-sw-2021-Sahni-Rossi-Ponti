26706a59dceb46d771608aa9d46a857262b8f2a5
Kunal Sahni <sahnikunalk13@gmail.com>
2021-04-24 12:16:27 +0200

    Player now implements Comparable instead of Comparator

--- src/main/java/it/polimi/ingsw/Server/Model/Player.java
+++ src/main/java/it/polimi/ingsw/Server/Model/Player.java
@@ -18,7 +18,7 @@ import it.polimi.ingsw.server.model.personalboardpackage.PersonalBoard;
  * This class represents a Player
  */
 
-public class Player implements Comparator<Player>{
+public class Player implements Comparable<Player>{
     private final Game game;
     private final String nickname;
     private int position;
@@ -235,17 +235,6 @@ public class Player implements Comparator<Player>{
         isPlayersTurn = false;
     }
 
-    /**
-     * Compares two players based on their position in the game
-     * @param firstPlayer the first Player to be compared
-     * @param secondPlayer the second Player to be compared
-     * @return the value 0 if firstPlayer is in the same position as secondPlayer; a value less than 0 if firstPlayer is before secondPlayer; and a value greater than 0 firstPlayer is after secondPlayer
-     */
-    @Override
-    public int compare(Player firstPlayer, Player secondPlayer) {
-        return Integer.compare(firstPlayer.getPosition(), secondPlayer.getPosition());
-    }
-
     public void setRank(int rank) {
         this.rank = rank;
     }
@@ -284,5 +273,49 @@ public class Player implements Comparator<Player>{
 
     public Map<MarketMarble, Integer> getTempMarbles() {
         return Map.copyOf(tempMarbles);
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure
+     * {@code sgn(x.compareTo(y)) == -sgn(y.compareTo(x))}
+     * for all {@code x} and {@code y}.  (This
+     * implies that {@code x.compareTo(y)} must throw an exception iff
+     * {@code y.compareTo(x)} throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
+     * {@code x.compareTo(z) > 0}.
+     *
+     * <p>Finally, the implementor must ensure that {@code x.compareTo(y)==0}
+     * implies that {@code sgn(x.compareTo(z)) == sgn(y.compareTo(z))}, for
+     * all {@code z}.
+     *
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
+     * class that implements the {@code Comparable} interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     *
+     * <p>In the foregoing description, the notation
+     * {@code sgn(}<i>expression</i>{@code )} designates the mathematical
+     * <i>signum</i> function, which is defined to return one of {@code -1},
+     * {@code 0}, or {@code 1} according to whether the value of
+     * <i>expression</i> is negative, zero, or positive, respectively.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(Player player) {
+        return this.getPosition() - player.getPosition();
     }
 }
