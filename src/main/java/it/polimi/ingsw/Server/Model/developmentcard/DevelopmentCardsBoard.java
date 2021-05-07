39176f8786785ec916c0b58a874f079dc8065b84
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-05-07 12:03:16 +0200

    DevelopmentCardsBoard class updated to fit changesHandler framework.

--- src/main/java/it/polimi/ingsw/Server/Model/developmentcard/DevelopmentCardsBoard.java
+++ src/main/java/it/polimi/ingsw/Server/Model/developmentcard/DevelopmentCardsBoard.java
@@ -1,31 +1,34 @@
 package it.polimi.ingsw.server.model.developmentcard;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.*;
-import java.util.stream.Collectors;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
-import static it.polimi.ingsw.server.model.developmentcard.Color.*;
-import static it.polimi.ingsw.server.model.developmentcard.Level.*;
+import it.polimi.ingsw.server.model.utils.ChangesHandler;
 
 public class DevelopmentCardsBoard {
-
+    private final ChangesHandler changesHandler;
     private final DevelopmentCardsDeck[][] board;
 
 
     /**
      * Creates Development cards board loading all cards from a json file
      * Cards are put in a matrix of decks,
-     * all level1 cards are in decks in the row 2, level2 cards are in the decks in the row 1, level 3 cards are in the deck in the row 0
-     * green cards are in the decks in the column 0, blue cards are in the decks in column 1, yellow cards are in the decks in column 2, purple cards are in the decks in column 3
+     * all level1 cards are in decks in the row 2, level2 cards are in the decks in the row 1,
+     * level 3 cards are in the deck in the row 0
+     * green cards are in the decks in the column 0, blue cards are in the decks in column 1,
+     * yellow cards are in the decks in column 2, purple cards are in the decks in column 3
      */
-    public DevelopmentCardsBoard() {
-        board = new DevelopmentCardsDeck[3][4];
-        loadDevelopmentCards();
+    public DevelopmentCardsBoard(ChangesHandler changesHandler) throws FileNotFoundException {
+        this.changesHandler = changesHandler;
+        this.board = new DevelopmentCardsDeck[3][4];
+        int i = 2, j = 0;
+        for (Level l : Level.values()) {
+            for (Color c : Color.values()) {
+                board[i][j] = new DevelopmentCardsDeck(changesHandler, c, l);
+                j++;
+            }
+            j=0;
+            i--;
+        }
     }
 
     /**
@@ -49,49 +52,6 @@ public class DevelopmentCardsBoard {
         return board[line][column].pop();
     }
 
-    /**
-    * Loads the cards from a json file and creates the related objects.
-     * Then it creates all the needed decks by calling the constructor and passing them the cards they need.
-    */
-    private void loadDevelopmentCards() {
-        List<DevelopmentCard> level1;
-        List<DevelopmentCard> level2;
-        List<DevelopmentCard> level3;
-
-        try {
-            JsonReader reader = new JsonReader(new FileReader(new File("src/main/resources/DevelopmentCards.json")));
-            DevelopmentCard[] developmentCards = new Gson().fromJson(reader, DevelopmentCard[].class);
-
-            level1 = Arrays.stream(developmentCards)
-                    .filter( developmentCard -> LEVEL1.equals(developmentCard.getLevel()))
-                    .collect(Collectors.toList());
-
-            board[2][0] = new DevelopmentCardsDeck(level1.stream().filter( developmentCard -> GREEN.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[2][1] = new DevelopmentCardsDeck(level1.stream().filter( developmentCard -> BLUE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[2][2] = new DevelopmentCardsDeck(level1.stream().filter( developmentCard -> YELLOW.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[2][3] = new DevelopmentCardsDeck(level1.stream().filter( developmentCard -> PURPLE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-
-            level2 = Arrays.stream(developmentCards)
-                    .filter( developmentCard -> LEVEL2.equals(developmentCard.getLevel()))
-                    .collect(Collectors.toList());
-
-            board[1][0] = new DevelopmentCardsDeck(level2.stream().filter( developmentCard -> GREEN.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[1][1] = new DevelopmentCardsDeck(level2.stream().filter( developmentCard -> BLUE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[1][2] = new DevelopmentCardsDeck(level2.stream().filter( developmentCard -> YELLOW.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[1][3] = new DevelopmentCardsDeck(level2.stream().filter( developmentCard -> PURPLE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-
-            level3 = Arrays.stream(developmentCards)
-                    .filter( developmentCard -> LEVEL3.equals(developmentCard.getLevel()))
-                    .collect(Collectors.toList());
-
-            board[0][0] = new DevelopmentCardsDeck(level3.stream().filter( developmentCard -> GREEN.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[0][1] = new DevelopmentCardsDeck(level3.stream().filter( developmentCard -> BLUE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[0][2] = new DevelopmentCardsDeck(level3.stream().filter( developmentCard -> YELLOW.equals(developmentCard.getColor())).collect(Collectors.toList()));
-            board[0][3] = new DevelopmentCardsDeck(level3.stream().filter( developmentCard -> PURPLE.equals(developmentCard.getColor())).collect(Collectors.toList()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * @return the first deck development card of color and level specified not picking it
