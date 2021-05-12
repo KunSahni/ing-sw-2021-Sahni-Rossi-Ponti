f6502e525a58d3e474d3361a7679e9016c02a489
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-05-12 14:40:22 +0200

    Removed old Player/Game-Actions

--- src/main/java/it/polimi/ingsw/Server/controller/message/action/gameaction/GameAction.java
+++
@@ -1,6 +0,0 @@
-package it.polimi.ingsw.server.controller.message.action.gameaction;
-
-import it.polimi.ingsw.server.controller.message.action.Action;
-
-public abstract class GameAction implements Action {
-}
