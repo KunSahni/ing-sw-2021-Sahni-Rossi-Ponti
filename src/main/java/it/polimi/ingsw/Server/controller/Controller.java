782300ace49f8291dc6e40029762e381b9b5d2f5
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-05-15 11:18:13 +0200

    Implemented Controller and RemoteView methods.

--- src/main/java/it/polimi/ingsw/Server/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/Server/controller/Controller.java
@@ -1,61 +1,65 @@
 package it.polimi.ingsw.server.controller;
 
-import it.polimi.ingsw.server.controller.gamepackage.Game;
-import it.polimi.ingsw.server.controller.message.action.Action;
-import it.polimi.ingsw.server.controller.message.action.gameaction.GameAction;
-import it.polimi.ingsw.server.controller.message.action.playeraction.PlayerAction;
+import it.polimi.ingsw.server.controller.action.Action;
+import it.polimi.ingsw.server.controller.action.gameaction.GameAction;
+import it.polimi.ingsw.server.controller.action.playeraction.InvalidActionException;
+import it.polimi.ingsw.server.controller.action.playeraction.PlayerAction;
+import it.polimi.ingsw.server.model.Game;
+import it.polimi.ingsw.server.remoteview.RemoteView;
 
 import java.util.Optional;
 import java.util.concurrent.Flow.*;
 
 public class Controller implements Subscriber<PlayerAction> {
     private final Game game;
+    private RemoteView remoteView;
     private Subscription subscription;
 
     public Controller(Game game) {
         this.game = game;
     }
 
-    public void endGame() {
-        // figure out how it will be called by EndTurnAction
+    public void setRemoteView(RemoteView remoteView) {
+        this.remoteView = remoteView;
     }
 
     private void handleGameAction(GameAction gameAction) {
-        // decide how to handle concurrent GameActions
         Action currentAction = gameAction;
         while (currentAction != null) {
             currentAction = currentAction.execute();
         }
-        // endGame call condition
     }
 
     private void handlePlayerAction(PlayerAction playerAction) {
-        if (playerAction.isAllowed()) {
-            Optional<GameAction> consequentGameAction = Optional.ofNullable(playerAction.execute());
-            consequentGameAction.ifPresent(this::handleGameAction);
-        } else {
-            // plan how to communicate to the player that the action that was
-            // sent to the serer is illegal
+        try {
+            playerAction.runChecks();
+            Optional<GameAction> consequentAction = Optional.ofNullable(playerAction.execute());
+            consequentAction.ifPresent(this::handleGameAction);
+        } catch (InvalidActionException e) {
+            remoteView.raiseError(playerAction.getNickname(), e.getMessage());
         }
     }
 
     @Override
     public void onSubscribe(Subscription subscription) {
         this.subscription = subscription;
+        this.subscription.request(1);
     }
 
     @Override
     public void onNext(PlayerAction playerAction) {
-
+        playerAction.setGame(game);
+        handlePlayerAction(playerAction);
+        this.subscription.request(1);
     }
 
     @Override
     public void onError(Throwable throwable) {
-
+        System.out.println("Error occurred: " + throwable.getMessage());
     }
 
     @Override
     public void onComplete() {
-
+        System.out.println("Controller is not subscribed to RemoteView anymore.");
     }
 }
--- src/main/java/it/polimi/ingsw/Server/remoteview/RemoteView.java
+++ src/main/java/it/polimi/ingsw/Server/remoteview/RemoteView.java
@@ -1,48 +1,51 @@
 package it.polimi.ingsw.server.remoteview;
 
+import it.polimi.ingsw.network.message.renderable.ErrorMessage;
+import it.polimi.ingsw.network.message.renderable.PrivateRenderable;
+import it.polimi.ingsw.network.message.renderable.Renderable;
+import it.polimi.ingsw.server.Connection;
 import it.polimi.ingsw.server.controller.Controller;
-import it.polimi.ingsw.server.controller.gamepackage.Game;
-import it.polimi.ingsw.server.controller.action.Action;
 import it.polimi.ingsw.server.controller.action.playeraction.PlayerAction;
-import it.polimi.ingsw.server.model.Player;
-import it.polimi.ingsw.server.network.renderable.ErrorMessage;
-import it.polimi.ingsw.server.network.renderable.PrivateRenderable;
-import it.polimi.ingsw.server.network.renderable.Renderable;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.*;
 import java.util.concurrent.SubmissionPublisher;
 
-public class RemoteView implements Flow.Subscriber<Renderable> {
+public class RemoteView implements Subscriber<Renderable> {
     /**
      * Utility class created to submit to the Game instance all player's
      * Actions received from Connection instances
      */
-    private class NetworkMessageForwarder implements Flow.Subscriber<PlayerAction> {
-        @Override
-        public void onSubscribe(Flow.Subscription subscription) {
+    public class NetworkMessageForwarder implements Subscriber<PlayerAction> {
+        private Subscription connectionSubscription;
 
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            connectionSubscription = subscription;
+            connectionSubscription.request(1);
         }
 
         @Override
-        public void onNext(Action item) {
-            item.setPlayer();
+        public void onNext(PlayerAction item) {
+            submissionPublisher.submit(item);
+            connectionSubscription.request(1);
         }
 
         @Override
         public void onError(Throwable throwable) {
-
+            System.out.println("Error occurred: " + throwable.getMessage());
         }
 
         @Override
         public void onComplete() {
-
+            System.out.println("MessageForwarder is not subscribed to connection anymore.");
         }
     }
 
-    private final Map<Player, Connection> connectedPlayers;
+    private final Map<String, Connection> connectedPlayers;
     private final SubmissionPublisher<PlayerAction> submissionPublisher;
+    private Subscription subscription;
 
     /**
      * Creates a RemoteView Object and subscribes to it the Game
@@ -50,8 +53,8 @@ public class RemoteView implements Flow.Subscriber<Renderable> {
      * @param controller Controller that will subscribe to the RemoteView.
      */
     public RemoteView(Controller controller) {
-        connectedPlayers = new HashMap<Player, Connection>();
-        submissionPublisher = new SubmissionPublisher<Action>();
+        connectedPlayers = new HashMap<>();
+        submissionPublisher = new SubmissionPublisher<>();
         submissionPublisher.subscribe(controller);
     }
 
@@ -59,12 +62,12 @@ public class RemoteView implements Flow.Subscriber<Renderable> {
      * Adds a player to the pool of players connected to the RemoteView.
      * The newly added players will now receive all BroadcastRenderables
      * and its PrivateRenderables via the network.
-     * @param player game participant.
+     * @param nickname game participant.
      * @param connection Connection object from which the player sends
      *                   and receives data to and from the server.
      */
-    public void addConnectedPlayer(Player player, Connection connection) {
-        connectedPlayers.put(player, connection);
+    public void addConnectedPlayer(String nickname, Connection connection) {
+        connectedPlayers.put(nickname, connection);
         connection.subscribe(new NetworkMessageForwarder());
     }
 
@@ -72,50 +75,53 @@ public class RemoteView implements Flow.Subscriber<Renderable> {
      * Unlinks a player that disconnected from the server, removing him
      * from the connected players list and effectively unsubscribing him
      * from model updates.
-     * @param player disconnected player.
+     * @param nickname disconnected player.
      */
-    public void removeDisconnectedPlayer(Player player) {
-        connectedPlayers.remove(player);
+    public void removeDisconnectedPlayer(String nickname) {
+        connectedPlayers.remove(nickname);
     }
 
     /**
      * Sends a generic error generic error message to the Player passed
      * as parameter.
-     * @param player target Player.
+     * @param nickname target Player.
      * @param errorText error text which will reach the Client.
      */
-    public void raiseError(Player player, String errorText) {
-        connectedPlayers.get(player).send(new ErrorMessage(errorText));
+    public void raiseError(String nickname, String errorText) {
+        connectedPlayers.get(nickname).send(new ErrorMessage(errorText));
     }
 
-    @Override
-    public void onSubscribe(Flow.Subscription subscription) {
 
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
     }
 
     /**
      * Listener for model changes. Forwards all the passed items
      * to each Client's View
-     * @param item
+     * @param item model update
      */
     @Override
     public void onNext(Renderable item) {
         if(item instanceof PrivateRenderable) {
-            connectedPlayers.get(item.getPlayer()).send(item);
+            connectedPlayers.get(((PrivateRenderable) item).getNickname()).send(item);
         } else {
             for (Connection connection : connectedPlayers.values()) {
                 connection.send(item);
             }
         }
+        subscription.request(1);
     }
 
     @Override
     public void onError(Throwable throwable) {
-
+        System.out.println("Error occurred: " + throwable.getMessage());
     }
 
     @Override
     public void onComplete() {
-
+        System.out.println("RemoteView is not subscribed to model anymore.");
     }
 }
