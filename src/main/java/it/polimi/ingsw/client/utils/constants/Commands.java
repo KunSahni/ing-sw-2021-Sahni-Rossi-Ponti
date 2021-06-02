492136750fed3d133263eb4a7eca62d450dfc6b9
Kunal Sahni <sahnikunalk13@gmail.com>
2021-06-02 12:46:28 +0200

    Added enum class for possible commands

---
+++ src/main/java/it/polimi/ingsw/client/utils/constants/Commands.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.client.utils.constants;
+
+public enum Commands {
+    PERSONAL_BOARD("show personal board"),
+    COMMONS("show commons"),
+    PRODUCE("produce"),
+    BUY_DEVELOPMENT_CARD("buy"),
+    TAKE_FROM_MARKET("take"),
+    ACTIVATE_LEADER_CARD("activate"),
+    DISCARD_LEADER_CARD("discard"),
+    HELP("help"),
+    QUIT("quit");
+
+    private final String command;
+
+    Commands(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
