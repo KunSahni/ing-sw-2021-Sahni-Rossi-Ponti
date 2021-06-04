e75fc8c0efc781cffa22cbdd21ac94d944832d11
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-06-04 07:39:10 +0200

    Implemented lobby size selection buttons.

--- src/main/java/it/polimi/ingsw/client/gui/guicontrollers/footer/PlayerSelectionFooterController.java
+++ src/main/java/it/polimi/ingsw/client/gui/guicontrollers/footer/PlayerSelectionFooterController.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.client.gui.guicontrollers.footer;
 
-import javafx.event.ActionEvent;
+import it.polimi.ingsw.network.message.messages.CreateLobbyMessage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -15,27 +15,30 @@ public class PlayerSelectionFooterController extends FooterController {
     private Button fourPlayersButton;
 
     @FXML
-    private void playSingle(ActionEvent actionEvent) {
+    private void playSingle() {
         sendCreateLobbyMessage(1);
     }
 
     @FXML
-    private void playTwo(ActionEvent actionEvent) {
+    private void playTwo() {
         sendCreateLobbyMessage(2);
     }
 
     @FXML
-    private void playThree(ActionEvent actionEvent) {
+    private void playThree() {
         sendCreateLobbyMessage(3);
     }
 
     @FXML
-    private void playFour(ActionEvent actionEvent) {
+    private void playFour() {
         sendCreateLobbyMessage(4);
     }
 
 
     private void sendCreateLobbyMessage(int size) {
-
+        parentController.getGui().getClientSocket().sendMessage(new CreateLobbyMessage(size));
+        if (size == 1) {
+            parentController.setLoadingFooterText("Setting up game");
+        }
     }
 }
