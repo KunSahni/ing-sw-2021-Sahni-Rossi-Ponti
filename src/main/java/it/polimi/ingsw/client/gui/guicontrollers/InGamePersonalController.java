7a1e22b2861b85c8457d88c2e197099ffa2c68cc
Vittorio Rossi <vittorio4.rossi@mail.polimi.it>
2021-06-05 16:28:12 +0200

    Introduced loading game GUI functionality, still unstable.

---
+++ src/main/java/it/polimi/ingsw/client/gui/guicontrollers/InGamePersonalController.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.gui.guicontrollers;
+
+public class InGamePersonalController extends JFXController{
+}
--- src/main/java/it/polimi/ingsw/client/gui/guicontrollers/footer/NicknameFooterController.java
+++ src/main/java/it/polimi/ingsw/client/gui/guicontrollers/footer/NicknameFooterController.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.client.gui.guicontrollers.footer;
 
 import it.polimi.ingsw.client.gui.FXMLResources;
 import it.polimi.ingsw.network.message.messages.AuthenticationMessage;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,11 +13,11 @@ public class NicknameFooterController extends FooterController {
     private Button joinButton;
 
     @FXML
-    private void sendAuthReply(ActionEvent actionEvent) {
+    private void sendAuthReply() {
         int gameId = parentController.getGui().getDumbModel().getGameID();
-        parentController.getGui().getClientSocket()
-                .sendMessage(new AuthenticationMessage(nicknameField.getText(), gameId));
         parentController.setFooter(FXMLResources.LOADING_FOOTER);
         parentController.setLoadingFooterText(gameId == -1 ? "joining lobby" : "joining game " + gameId);
+        parentController.getGui().getClientSocket()
+                .sendMessage(new AuthenticationMessage(nicknameField.getText(), gameId));
     }
 }
