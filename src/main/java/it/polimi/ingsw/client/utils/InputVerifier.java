1f1c3d2094674fe23698e99e0d1cae64b9c9e411
Kunal Sahni <sahnikunalk13@gmail.com>
2021-06-05 19:32:29 +0200

    implemented some controls in InputVerifier

--- src/main/java/it/polimi/ingsw/client/utils/InputVerifier.java
+++ src/main/java/it/polimi/ingsw/client/utils/InputVerifier.java
@@ -5,11 +5,13 @@ import it.polimi.ingsw.client.utils.dumbobjects.DumbModel;
 import it.polimi.ingsw.server.controller.action.playeraction.PlayerAction;
 import it.polimi.ingsw.server.model.developmentcard.Color;
 import it.polimi.ingsw.server.model.developmentcard.Level;
+import it.polimi.ingsw.server.model.market.MarketMarble;
 import it.polimi.ingsw.server.model.utils.ExecutedActions;
 import it.polimi.ingsw.server.model.utils.GameState;
 import it.polimi.ingsw.server.model.utils.ProductionCombo;
 import it.polimi.ingsw.server.model.utils.Resource;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,14 +26,34 @@ public class InputVerifier {
         this.dumbModel = dumbModel;
     }
 
+    /**
+     * Checks if user can activate a production
+     * @param chosenProductionCombo a production combo which includes all choices made by user
+     * @return true if request is valid, false otherwise
+     */
     public boolean canProduce(ProductionCombo chosenProductionCombo){
         return true;
     }
 
+    /**
+     * Checks if user can take resource from market
+     * @param place
+     * @param index
+     * @return true if request is valid, false otherwise
+     */
     public boolean canTake(String place, int index){
-        return true;
+        return canDoAction(ExecutedActions.STORED_TEMP_MARBLES_ACTION);
     }
 
+    /**
+     * Checks if user can buy the selected development card
+     * @param chosenLevel level of chosen development card
+     * @param chosenColor color of chosen development card
+     * @param developmentCardSlotIndex index of development card slot where user wants to place new card
+     * @param depotsResources map of resources which will be discarded from depots or store leader cards
+     * @param strongboxResources map of resources which will be discarded from strongbox
+     * @return true if request is valid, false otherwise
+     */
     public boolean canBuy(Level chosenLevel, Color chosenColor, int developmentCardSlotIndex, Map<Resource, Integer> depotsResources, Map<Resource, Integer> strongboxResources){
         return legalResourcesChoice(dumbModel.getOwnPersonalBoard().getDepots().getStoredResources(), depotsResources)
                 && legalResourcesChoice(dumbModel.getOwnPersonalBoard().getStrongbox().getStoredResources(), strongboxResources)
@@ -39,18 +61,33 @@ public class InputVerifier {
                 //&& dumbModel.getDevelopmentCardsBoard().peekCard(chosenLevel, chosenColor).getCost()
     }
 
+    /**
+     * Checks if user can discard selected leader card
+     * @param index the index of the leader card which he wants to discard
+     * @return true if request is valid, false otherwise
+     */
     public boolean canActivate(int index){
         return dumbModel.getOwnPersonalBoard().getLeaderCards().get(index) != null
                 && !dumbModel.getOwnPersonalBoard().getLeaderCards().get(index).isActive()
                 && canDoAction(ExecutedActions.ACTIVATED_LEADER_CARD_ACTION);
     }
 
+    /**
+     * Checks if user can discard selected leader card
+     * @param index the index of the leader card which he wants to discard
+     * @return true if request is valid, false otherwise
+     */
     public boolean canDiscard(int index){
         return dumbModel.getOwnPersonalBoard().getLeaderCards().get(index) != null
                 && !dumbModel.getOwnPersonalBoard().getLeaderCards().get(index).isActive()
                 && canDoAction(ExecutedActions.DISCARDED_LEADER_CARD_ACTION);
     }
 
+    /**
+     * Checks if selected resources in pregame are correct
+     * @param pickedResources resource chosen by user in pregame
+     * @return true if request is valid, false otherwise
+     */
     public boolean canPickResources(Map<Resource, Integer> pickedResources){
         return pickedResources
                 .values()
@@ -61,21 +98,53 @@ public class InputVerifier {
                 .sum() == dumbModel.getOwnPersonalBoard().getPosition()/2;
     }
 
+    /**
+     * Checks if the selected leader cards are correct
+     * @param chosenLeaderCards leader cards chosen by user
+     * @return true if request is valid, false otherwise
+     */
     public boolean canPickLeaderCards(List<DumbLeaderCard> chosenLeaderCards){
         return chosenLeaderCards.size()==2 &&
                 dumbModel.getTempLeaderCards().containsAll(chosenLeaderCards) &&
                 dumbModel.getGameState().equals(GameState.DEALT_LEADER_CARDS);
     }
 
-    public boolean canPickTempMarbles(){
+    /**
+     * Checks if the chosen market marbles are a valid selection
+     * @param chosenMarbles map of chosen market marbles
+     * @return true if request is valid, false otherwise
+     */
+    public boolean canPickTempMarbles(Map<MarketMarble, Integer> chosenMarbles){
         return true;
     }
 
+    /**
+     * Checks if nextAction is valid based on turn action order logic
+     * @param nextAction the nextAction which user wants to execute
+     * @return true if it can be executed in that order, false otherwise
+     */
     private boolean canDoAction(ExecutedActions nextAction){
-        List<ExecutedActions> executedActionsList;
-        return true;
+        if(!dumbModel.getOwnPersonalBoard().getTurnStatus())
+            return false;
+        ExecutedActions mostRecentAction = dumbModel.getTurnActions().get(dumbModel.getTurnActions().size() - 1);
+        if (mostRecentAction.equals(ExecutedActions.TURN_ENDED_ACTION)) {
+            return false;
+        } else if (mostRecentAction.equals(ExecutedActions.STORED_TEMP_MARBLES_ACTION)) {
+            return nextAction.equals(ExecutedActions.STORED_MARKET_RESOURCES_ACTION);
+        } else {
+            if (nextAction.isCompulsory())
+                return dumbModel.getTurnActions().stream().noneMatch(ExecutedActions::isCompulsory);
+            else if (nextAction.equals(ExecutedActions.TURN_ENDED_ACTION))
+                return dumbModel.getTurnActions().stream().anyMatch(ExecutedActions::isCompulsory);
+            else return true;
+        }
     }
 
+    /**
+     * Checks if selected index by user is valid
+     * @param index selected index by user
+     * @return true if the index is valid, false otherwise
+     */
     public boolean canSeePersonalBoard(int index){
         return index>0 && index<=dumbModel.getSize();
     }
